fcfe33d9e2fc8fa7e4f11266c2e64b02c296d2ee
Parsa Noroozi <parsan.6347@gmail.com>
2021-01-12 22:25:22 +0330

    commit 8

--- GT.ts
+++ GT.ts
@@ -5,7 +5,7 @@ function strlen(s:string):number{
 
 var s = "its a git test";
 console.log(strlen(s+1));
-console.log("bullshit");
+
 console.log("from me side");
 console.log("mate Side");
 //console.log("hello");
